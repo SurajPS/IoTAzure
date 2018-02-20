64cdc67492198e00a939d1d50ec7463d2d1c449b
Suraj PS <mukundsuraj@gmail.com>
2018-02-20 16:55:19 +0100

    vizIoT15

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,3 @@
-'use strict';
-
-var EventHubClient = require('azure-event-hubs').Client;
-var connectionString = 'HostName=NodeHub.azure-devices.net;DeviceId=emulateddevice2;SharedAccessKey=KKhT5XWAYel1x+VC1Er+eG5zeK8k5sL6TLJYM1XWQ8I=';
-var printError = function (err) {
-    console.log(err.message);
-};
-
-var printMessage = function (message) {
-    console.log('Message received: ');
-    console.log(JSON.stringify(message.body));
-    console.log('');
-};
-var client = EventHubClient.fromConnectionString(connectionString);
-
-
-
-
 //Main program part
 
 
