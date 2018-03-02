94f9d1811518a9767685e48dee2935cdcccf746e
Suraj PS <mukundsuraj@gmail.com>
2018-03-02 18:25:11 +0100

    vizT48

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
 
     var ws = new WebSocket('wss://' + location.host);
     ws.onopen = function () {
-                  console.log('Successfully connected WebSocket:P');
+                  console.log('Successfully connected WebSocket:Q');
                   console.log(ws);
     }
     ws.onmessage = function (message) {
