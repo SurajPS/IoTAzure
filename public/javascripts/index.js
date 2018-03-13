2b2968f03332865253bc32f233bc161a870c1fc5
Suraj PS <mukundsuraj@gmail.com>
2018-03-13 10:43:36 +0100

    C5

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -53,7 +53,7 @@ function sizeDec(){
 $(document).ready(function () {
     var ws = new WebSocket('wss://' + location.host);
     ws.onopen = function () {
-                  console.log('Successfully connected WebSocket:B');
+                  console.log('Successfully connected WebSocket:C');
                   console.log(ws);
     }
                   d3lineChart();
